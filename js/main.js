05521dd29b142657d1df0667757d8a339151ec6f
Guany <1084072690@qq.com>
2022-10-03 22:27:19 +0800

    feat: can play

--- js/main.js
+++ js/main.js
@@ -1,26 +1,51 @@
-const game = document.querySelector('.game')
-const ul = game.querySelector('ul')
+const checkerboard = document.querySelector('.checkerboard')
+const ul = checkerboard.querySelector('ul')
 
 let count = 36
 let str = ''
 
-let foods = ['🍕', '🍔', '🍟', '🌭', '🍿', '🧂', '🥓', '🥚', '🍳', '🧇']
+let foods = ['🍕', '🍔', '🍟', '🌭', '🍿', '🧂', '🥓', '🥚']
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max)
 }
 
 for (let i = 0; i < count; i++) {
-  str += `<li>${foods[getRandomInt(10)]}</li>`
+  str += `<li>${foods[getRandomInt(8)]}</li>`
 }
 
 ul.innerHTML = str
 
 const lis = ul.querySelectorAll('li')
 
+let first
+let second
+
 lis.forEach(li => {
   li.addEventListener('click', e => {
-    // console.log(e.target.textContent)
-    e.target.classList.add('clear')
+    if (!e.target.classList.contains('checked')) {
+      if (!first) {
+        first = e.target
+        first.classList.add('checked')
+      } else {
+        second = e.target
+        second.classList.add('checked')
+
+        if (second.textContent === first.textContent) {
+          first.classList.remove('checked')
+          second.classList.remove('checked')
+
+          first.classList.add('clear')
+          second.classList.add('clear')
+
+          first = undefined
+          second = undefined
+        } else {
+          first.classList.remove('checked')
+          first = second
+          second = undefined
+        }
+      }
+    }
   })
 })
